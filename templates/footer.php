06760550d9f0fcd9da8325517a92be8bd0079f08
CUANMOLOGY <amdrewijaya21@gmail.com>
2025-10-22 18:26:33 +0700

    add copy right in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,9 @@
 <?php
 ?>
-    </div> </body>
+    </div> <footer style="text-align: center; padding: 20px 0; margin-top: 40px; border-top: 1px solid #ddd; font-size: 0.9em; color: #777;">
+        <p>
+            Copyright &copy; <?php echo date("Y"); ?> Created by Andre Wijaya. 221011400791. 07TPLP019. All rights reserved.
+        </p>
+    </footer>
+    </body>
 </html>
